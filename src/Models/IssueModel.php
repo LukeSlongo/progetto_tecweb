be47e2974ebfdf96098d3b6a20af794ad898f80b
enriquehernandezgris <quiquepro1691@gmail.com>
2026-06-25 14:23:24 +0200

    ho dimenticato fare branch e pr per questo mi dispiace, ma solamente ho cambiato la icona e che utente studente puo vedere issue List

--- src/Models/IssueModel.php
+++ src/Models/IssueModel.php
@@ -166,7 +166,6 @@ class IssueModel extends Model
         $sql = "UPDATE {$this->table} SET room_id = ?, title = ?, description = ? WHERE id = ?";
         return $this->query($sql, [$room_id, $title, $description, $issue_id]);
     }
-    
     public function getIssuesByTechnician($technician_id)
     {
         $sql = "SELECT 
